d3d1023b0ad0d95f5a2569be96ba94cd37851294
ychakir <chakirpay@gmail.com>
2021-06-22 20:06:42 +0100

    add mongodb to sav biomedical batch

--- src/main/java/ma/tiwtiw/savBiomedical/service/impl/TranslationServiceImpl.java
+++ src/main/java/ma/tiwtiw/savBiomedical/service/impl/TranslationServiceImpl.java
@@ -4,7 +4,9 @@ import java.util.List;
 import ma.tiwtiw.core.service.impl.BaseMongoTemplateServiceImpl;
 import ma.tiwtiw.savBiomedical.model.Translation;
 import ma.tiwtiw.savBiomedical.service.TranslationService;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class TranslationServiceImpl extends
 
   @Override
   protected MongoTemplate getMongoTemplate() {
+    mongoTemplate.indexOps("translation").ensureIndex(new Index("code", Direction.ASC).unique());
     return mongoTemplate;
   }
 
